608b2af537ab6d499dc1c26837e5c1755ee26c85
lekseich8 <lekseich8@gmail.com>
2024-01-13 11:15:44 +0300

    Init User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
