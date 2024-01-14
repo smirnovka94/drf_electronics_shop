3ceb129dbbb6807bb3168d3457a8edb70717bc4c
lekseich8 <lekseich8@gmail.com>
2024-01-14 17:26:27 +0300

    Fixed Validator

--- shop_link/validators.py
+++ shop_link/validators.py
@@ -10,9 +10,9 @@ class Status_Link_In_LinkValidator:
             raise ValidationError("У завода не может быть поставщика")
 
 class Debt_In_LinkValidator:
-    """Запрет на ввод поставщика для завода"""
+    """Запрет на ввод долга поставщику для завода"""
     def __call__(self, attrs):
         status_link = attrs.get('status_link')
-        debt = attrs.get('related_link')
+        debt = attrs.get('debt')
         if status_link == "FA" and debt is not None:
             raise ValidationError("У завода не может быть задолженности перед поставщиком")
