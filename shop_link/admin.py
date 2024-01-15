8228a4fe0b79f85741f0e04fc9c097664a15e682
lekseich8 <lekseich8@gmail.com>
2024-01-15 22:31:05 +0300

    Добавил в админ-панели ссылку на «Поставщика»

--- shop_link/admin.py
+++ shop_link/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from shop_link.models import Link, Product
 
 
@@ -9,10 +10,19 @@ def make_published(modeladmin, request, queryset):
 
 @admin.register(Link)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'status_link', 'name', 'debt')
+    list_display = ('pk', 'status_link', 'name', 'related_link_link', 'debt')
     list_filter = ('city',)
     actions = [make_published]
 
+    def related_link_link(self, obj):
+        link = obj.related_link
+        if link:
+            return format_html('<a href="/admin/shop_link/link/{}/change/">{}</a>', link.pk, link.name)
+        else:
+            return None
+
+    related_link_link.allow_tags = True
+    related_link_link.short_description = 'Related Link'
 
 @admin.register(Product)
 class ContactAdmin(admin.ModelAdmin):
